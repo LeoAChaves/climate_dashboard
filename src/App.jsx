eb1e4ba252bb50568cc4cbaf621c6fb6e26dc2c9
Leo <chaves.leonardoalmeida@gmail.com>
2026-05-13 15:51:08 -0300

    Remove unused Grafana config + legacy toolbar icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage";
-import GrafanaTesterPage from "./pages/GrafanaTesterPage"; // Importe a nova página aqui
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<DashboardPage />} />
-        <Route path="/grafanaTest" element={<GrafanaTesterPage />} />{" "}
       </Routes>
     </Router>
   );
